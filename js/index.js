1c8c1472baf438ff401dfd2bab10fd1de2c758d9
Christian Barrios <christiandbf@hotmail.com>
2017-05-01 13:00:28 -0500

    Ramdon quotes finished

---
+++ js/index.js
@@ -0,0 +1,21 @@
+function ajaxQuote() {
+    $.ajax({
+        url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+        success: function(data) {
+            var post = data.shift(); // The data is an array of posts. Grab the first one.
+            console.log(post.content);
+            $("#text-quote").empty().append(post.content);
+            $("#author-quote").empty().append(post.title);
+            },
+        cache: false
+    });
+};
+
+$(document).ready(ajaxQuote);
+
+$("#new-quote").on("click", ajaxQuote);
+
+$("#share-twitter").click(function() {
+    alert("button share twitter")
+});
+
