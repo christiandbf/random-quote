96a34069c0d582f317495011fea654cb6b044936
Christian Barrios <christiandbf@hotmail.com>
2017-05-03 09:55:17 -0500

    Finished

--- js/index.js
+++ js/index.js
@@ -3,10 +3,9 @@ function ajaxQuote() {
         url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
         success: function(data) {
             var post = data.shift(); // The data is an array of posts. Grab the first one.
-            console.log(post.content);
             $("#text-quote").empty().append(post.content);
-            $("#author-quote").empty().append(post.title);
-            },
+            $("#author-quote").empty().append(post.title);            
+        },
         cache: false
     });
 };
@@ -16,6 +15,8 @@ $(document).ready(ajaxQuote);
 $("#new-quote").on("click", ajaxQuote);
 
 $("#share-twitter").click(function() {
-    alert("button share twitter")
+    var textTweet = $("#text-quote p").text() + " - " + $("#author-quote").text();
+    var tweetLink = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(textTweet);
+    window.open(tweetLink, '_blank');
 });
 
